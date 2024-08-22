d7bd2752720a8261cf584dad309621e4630ce283
Noximus <hrichardson83@gmail.com>
2024-08-22 13:23:50 -0400

    baby steps - 30 days to laravel

---
+++ app/Models/Job.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Job
+{
+  public static function all(): array
+  {
+    return [
+      [
+        'id' => 1,
+        'title' => 'Frontend Developer',
+        'salary' => '$80,000',
+      ],
+      [
+        'id' => 2,
+        'title' => 'Backend Developer',
+        'salary' => '$90,000',
+      ],
+      [
+        'id' => 3,
+        'title' => 'Fullstack Developer',
+        'salary' => '$100,000',
+      ]
+    ];
+  }
+
+  public static function find(int $id): array
+  {
+    $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+    if (! $job) {
+      abort(404);
+    }
+
+    return $job;
+  }
+}
