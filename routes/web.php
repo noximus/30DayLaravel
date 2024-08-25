1bd67d6dd3b31daddfa46766e384c78e07a2dc56
Noximus <hrichardson83@gmail.com>
2024-08-25 00:14:09 -0400

    Mkae a login part 1 Episode 21

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,24 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::view('/contact', 'contact');
 
 Route::resource('jobs', JobController::class);
+
+// Auth
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+
+
+
 // Route::resource('jobs', JobController::class, [
 //     'only' => ['index', 'show', 'create', 'store']
 // ]);
