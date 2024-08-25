0a61ed73914506eae64a97fa8e5e713e6415cf95
Noximus <hrichardson83@gmail.com>
2024-08-25 10:59:09 -0400

    Make a Login Part 2 Episode 22

--- app/Http/Controllers/RegisterUserController.php
+++ app/Http/Controllers/RegisterUserController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterUserController extends Controller
 {
@@ -12,6 +15,27 @@ class RegisterUserController extends Controller
     }
     public function store()
     {
-        // todo
+        // ---------validate
+        $attributes = request()->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => ['required', 'email', 'max:254'],
+            // this will add a password_confirmation field to the request
+            'password' => ['required', Password::min(6), 'confirmed'],
+        ]);
+
+        // ---------create the user
+        $user = User::create($attributes);
+        // User::create([
+        //     'first_name' => request('first_name'),
+        //     'last_name' => request('last_name'),
+        //     'email' => request('email'),
+        //     'password' => bcrypt(request('password')),
+        // ])
+        // ---------login
+        Auth::login($user);
+
+        // ----------redirect
+        return redirect('/jobs');
     }
 }
--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -12,6 +15,25 @@ class SessionController extends Controller
     }
     public function store()
     {
-        // return view('auth.store');
+        // validate form
+        $attributes = request()->validate([
+            'email' => ['required', 'email', 'max:254'],
+            'password' => ['required', Password::min(6)],
+        ]);
+        //attempt to login
+        if (!Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verified.'
+            ]);
+        }
+        // regenerate the session token
+        request()->session()->regenerate();
+
+        return redirect('/jobs');
+    }
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect('/');
     }
 }
